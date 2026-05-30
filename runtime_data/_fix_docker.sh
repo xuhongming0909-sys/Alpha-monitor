e2a623b3d452a425dd0745fdfcde62869067ba3c
xuhongming0909-sys <xuhongming0909@gmail.com>
2026-05-30 09:20:23 +0800

    删除SearXNG自定义搜索引擎: 停容器, 删镜像, 删配置文件, 删mimo_agent模块, 完全依赖MiMo官方webSearchEnabled

--- runtime_data/_fix_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo python3 -c "import json; f=open('/etc/docker/daemon.json','w'); json.dump({'registry-mirrors':['https://docker.1ms.run','https://docker.xuanyuan.me']},f); f.close()"
-cat /etc/docker/daemon.json
-sudo systemctl restart docker
-echo "Docker status: $(systemctl is-active docker)"
